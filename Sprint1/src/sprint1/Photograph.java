07cb921c0da572897eef9fb75159ce70d51dd1ff
Adair Tabb <mtabb2@forum.montevallo.edu>
2021-01-28 15:48:46 -0500

    Photograph class progress so far.

--- Sprint1/src/sprint1/Photograph.java
+++ Sprint1/src/sprint1/Photograph.java
@@ -1,5 +1,34 @@
 package sprint1;
 
 public class Photograph {
+	private final String filename;
+	private final String caption;
+	
+	Photograph(String filename, String caption) {
+		this.filename = filename;
+		this.caption = caption;
+	}
+	
+	public String getFileName() {
+		return filename;
+	}
+	
+	public String getCaption() {
+		return caption;
+	}
+	
+	//public boolean equals(Object o) {
+		
+	//}
+	
+	public String toString() {
+		return "Filename: " + filename + "/n" + "Caption: " + caption;
+	}
+	
+	public static void Main(String args[]) {
+		Photograph p = new Photograph("DefaultFilename", "DefaultCaption");
+		p.toString();
 
+	}
+	
 }
